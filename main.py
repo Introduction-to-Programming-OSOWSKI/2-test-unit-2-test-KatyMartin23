2c06713df6da2dd31d909663157a92de5e45da6d
KatyMartin23 <832006@mystma.org>
2021-01-15 15:54:55 +0000

    Unit 2 test try 1

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-#WRITE YOUR CODE IN THIS FILE
+def waterstate(f):
+    if f <= 32:
+        return "solid"
+    elif f >= 212:
+        return "gas"
+    else:
+        return "liquid" 
+print(waterstate(60))
+
+def isDozen(d):
+    if d % 12 == 0:
+        return True
+    else:
+        return False
+print(isDozen(36))
+
+def toGerman(word):
+    if word == "yes":
+        return "ja"
+    else:
+        return "nein"
+print(toGerman("no"))
+
+def stoplight(c):
+    if c == "green":
+        return "go"
+    elif c == "yellow":
+        return "slow"
+    else:
+        return "stop"
+print(stoplight("green"))
+
+
+
